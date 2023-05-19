866a3f2c3d623a02d0ce61507dba851804510ed0
kylearmour1 <kylejarmour@gmail.com>
2023-05-18 17:02:30 -0700

    Defining GraphQL Schema and Resolvers

---
+++ server/graphql/resolvers/index.js
@@ -0,0 +1,16 @@
+const userResolvers = require('./userResolvers');
+const videoResolvers = require('./videoResolvers');
+const commentResolvers = require('./commentResolvers');
+
+module.exports = {
+    Query: {
+        ...userResolvers.Query,
+        ...videoResolvers.Query,
+        ...commentResolvers.Query,
+    },
+    Mutation: {
+        ...userResolvers.Mutation,
+        ...videoResolvers.Mutation,
+        ...commentResolvers.Mutation,
+    },
+};
