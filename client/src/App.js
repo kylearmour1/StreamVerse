2c1338ce24948d36362af155694111b2837850c5
Olive Provencio-Johnson <120212494+Olive-Provencio-Johnson@users.noreply.github.com>
2023-05-25 22:05:55 -0700

    API for app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,34 @@
 // App.js
-import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import { ApolloProvider } from '@apollo/client';
-import client from './apolloClient';
-import Login from './components/Login/Login';
-import Home from './components/Home/Home'; 
-
+import React, { useEffect } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { ApolloProvider } from "@apollo/client";
+import client from "./apolloClient";
+import Login from "./components/Login/Login";
+import Home from "./components/Home/Home";
+import axios from "axios";
 
 function App() {
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const apiKey = process.env.REACT_APP_API_KEY;
+        const apiUrl =
+          "https://www.googleapis.com/youtube/v3/search/videos?part=snippet&maxResults=25&q=react&key=" +
+          apiKey +
+          "&type=video";
+        const response = await axios.get(apiUrl, {
+          params: {
+            key: apiKey,
+          },
+        });
+        const data = response.data;
+        console.log(data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchData();
+  }, []);
   return (
     <ApolloProvider client={client}>
       <Router>
@@ -19,5 +40,5 @@ function App() {
     </ApolloProvider>
   );
 }
-
 export default App;
+       
