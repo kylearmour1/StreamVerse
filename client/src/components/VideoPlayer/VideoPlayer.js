1ac6f25291c888fae269609fd95073f0f4295406
Olive Provencio-Johnson <120212494+Olive-Provencio-Johnson@users.noreply.github.com>
2023-05-29 16:56:51 -0700

    added export statement to VideoPlayer component

--- client/src/components/VideoPlayer/VideoPlayer.js
+++ client/src/components/VideoPlayer/VideoPlayer.js
@@ -7,7 +7,7 @@ function VideoPlayer({ video }) {
     }
 // <iframe is used to display the YouTube video on our site without having a user be redirected to YouTube. It also has a set of allowances, such as allow autoplay, or allowFullScreen 
     return (
-        <div className="video-player-page">
+        <div className="video-player">
             <h2>{video.title}</h2>
             <iframe
                 src={`https://www.youtube.com/embed/${video.id}`}
@@ -16,5 +16,7 @@ function VideoPlayer({ video }) {
                 allowFullScreen
             />
         </div>
-    )
+    );
 }
+
+export default VideoPlayer;
