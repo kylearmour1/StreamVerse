d750569c456db0de49cd9373ac09ce3a47d27c58
jbungurait <120280286+jbungurait@users.noreply.github.com>
2023-06-06 21:09:48 -0700

    revert ports

--- server/server.js
+++ server/server.js
@@ -11,9 +11,9 @@ const SECRET = process.env.JWT_SECRET;
 
 const app = express();
 
-app.use(cors({
-  origin: 'https://127.0.0.1:3000' 
-}));
+// app.use(cors({
+//   origin: 'http://127.0.0.1:3000' 
+// }));
 
 const authMiddleware = (req, res, next) => {
   const token = req.headers.authorization;
@@ -51,7 +51,7 @@ async function startServer() {
 
   const PORT = process.env.PORT || 3001;
   app.listen(PORT, () => {
-    console.log(`Server is running at https://localhost:${PORT}${server.graphqlPath}`);
+    console.log(`Server is running at http://localhost:${PORT}${server.graphqlPath}`);
   });
 }
 
