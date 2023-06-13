0af95d747fb486a6ea139b7edfc0082c19919f33
jbungurait <120280286+jbungurait@users.noreply.github.com>
2023-06-12 21:11:25 -0700

    added links to github on Logout

--- client/src/components/Login/Logout.js
+++ client/src/components/Login/Logout.js
@@ -12,10 +12,17 @@ const Logout = () => {
             <h2>Thanks for visiting StreamVerse!</h2>
             <h2>Built by: </h2>
             <ul>
-                <li>Kyle Armour</li>
-                <li>Allyson Gonzales</li>
-                <li>Olive Provencio-Johnson</li>
-                <li>Josh Ungurait</li>
+                <li>
+                    <a  href="https://github.com/kylearmour1">Kyle Armour</a>
+                    </li>
+                <li>
+                    <a href="https://github.com/Ally27">Allyson Gonzales</a>
+                    </li>
+                <li>
+                    <a href="https://github.com/Olive-Provencio-Johnson">Olive Provencio-Johnson</a>
+                    </li>
+                <li><a href="https://github.com/jbungurait">Josh Ungurait</a>
+                </li>
             </ul>
             <Link className="btn btn-lg btn-info m-2" to="/login">
         Login
